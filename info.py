31b87093d41200f4b7a811a54c9e72796851c475
preethamdg <preethamdgpreethamdg5@gmail.com>
2025-01-26 03:45:11 -0800

    Update info.py

--- info.py
+++ info.py
@@ -77,11 +77,11 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 DOWNLOAD_LOCATION = environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/AudioBoT/")
 
 #chatgptAI
-AI = is_enabled((environ.get("AI","False")), False)
+AI = is_enabled((environ.get("AI","True")), False)
 OPENAI_API = environ.get("OPENAI_API", " ")
-DEEP_API = environ.get("DEEP_API", "")
-GOOGLE_API_KEY = environ.get("GOOGLE_API_KEY", "")
-AI_LOGS = int(environ.get("AI_LOGS", "-1002470010762")) #GIVE YOUR NEW LOG CHANNEL ID TO STORE MESSAGES THAT THEY SEARCH IN BOT PM.... [ i have added this to keep an eye on the users message, to avoid misuse of Bot ]
+DEEP_API = environ.get("DEEP_API", "3ac9b077-654f-45c6-a1f0-a04a5ef6b69e")
+GOOGLE_API_KEY = environ.get("GOOGLE_API_KEY", "AIzaSyD214hhYJ-xf8rfaWX044_g1VEBQ0ua55Q")
+AI_LOGS = int(environ.get("AI_LOGS", "-1001868871195")) #GIVE YOUR NEW LOG CHANNEL ID TO STORE MESSAGES THAT THEY SEARCH IN BOT PM.... [ i have added this to keep an eye on the users message, to avoid misuse of Bot ]
 
 #Auto approve 
 CHAT_ID = [int(app_chat_id) if id_pattern.search(app_chat_id) else app_chat_id for app_chat_id in environ.get('CHAT_ID', '').split()]
