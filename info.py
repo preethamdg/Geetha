35ca809148030b424b9c51f45d2c4386d94e21b9
preethamdg <preethamdgpreethamdg5@gmail.com>
2025-01-26 03:40:16 -0800

    Update info.py

--- info.py
+++ info.py
@@ -81,7 +81,7 @@ AI = is_enabled((environ.get("AI","False")), False)
 OPENAI_API = environ.get("OPENAI_API", " ")
 DEEP_API = environ.get("DEEP_API", "")
 GOOGLE_API_KEY = environ.get("GOOGLE_API_KEY", "")
-AI_LOGS = int(environ.get("AI_LOGS", "")) #GIVE YOUR NEW LOG CHANNEL ID TO STORE MESSAGES THAT THEY SEARCH IN BOT PM.... [ i have added this to keep an eye on the users message, to avoid misuse of Bot ]
+AI_LOGS = int(environ.get("AI_LOGS", "-1002470010762")) #GIVE YOUR NEW LOG CHANNEL ID TO STORE MESSAGES THAT THEY SEARCH IN BOT PM.... [ i have added this to keep an eye on the users message, to avoid misuse of Bot ]
 
 #Auto approve 
 CHAT_ID = [int(app_chat_id) if id_pattern.search(app_chat_id) else app_chat_id for app_chat_id in environ.get('CHAT_ID', '').split()]
